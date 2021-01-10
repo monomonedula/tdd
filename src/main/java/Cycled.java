cceb1d0ac8e46f7575833bb63293974f8f99b01a
monomonedula <valh@tuta.io>
2021-01-10 22:41:13 +0200

    refactored

--- src/main/java/Cycled.java
+++ src/main/java/Cycled.java
@@ -1,39 +1,40 @@
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.NoSuchElementException;
 
 public final class Cycled<X> extends IterableEnvelope<X>{
     public Cycled(Iterable<X> iterable) {
         super(
-                new Iterable<X>() {
-
-                    @Override
-                    public Iterator<X> iterator() {
-                        return new Iterator<X>() {
-                            final List<X> copy = new LinkedList<X>();
-                            final Iterator<X> origin = iterable.iterator();
-                            Iterator<X> current = null;
-
-                            @Override
-                            public boolean hasNext() {
-                                return this.origin.hasNext() || this.copy.size() > 0;
-                            }
-
-                            @Override
-                            public X next() {
-                                if (this.origin.hasNext()) {
-                                    X next = this.origin.next();
-                                    this.copy.add(next);
-                                    return next;
-                                }
-                                if (this.current == null || !this.current.hasNext()) {
-                                    this.current = this.copy.iterator();
-                                }
-                                return this.current.next();
-                            }
-                        };
-                    }
-                }
+                () -> new CycledIterator<>(iterable.iterator())
         );
+    }
+}
+
+class CycledIterator<X> implements Iterator<X> {
+    final List<X> copy = new LinkedList<X>();
+    final Iterator<X> origin;
+    Iterator<X> current = null;
+
+    public CycledIterator(Iterator<X> origin) {
+        this.origin = origin;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return this.origin.hasNext() || this.copy.size() > 0;
+    }
+
+    @Override
+    public X next() {
+        try {
+            X next = this.origin.next();
+            this.copy.add(next);
+            return next;
+        } catch (NoSuchElementException e) {
+            if (this.current == null || !this.current.hasNext())
+                this.current = this.copy.iterator();
+            return this.current.next();
+        }
     }
 }
